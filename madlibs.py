fd27ccb0d3e5ec0b8d725f9ef849041b83983a8f
gregopixel <61534420+gregopixel@users.noreply.github.com>
2020-09-11 10:55:35 +1000

    Added madlibs

---
+++ madlibs.py
@@ -0,0 +1,19 @@
+
+name = str(input("Name: "))
+relation = str(input("Relation: "))
+noun = str(input("Noun: "))
+animal = str(input("Animal (plural): "))
+sport = str(input("Sport: "))
+adjective1 = str(input("Adjective: "))
+adjective2 = str(input("Another Adjective: "))
+verb = str(input("Verb: "))
+
+print(f""" Dear {relation},
+Camp {noun} has been {adjective1} so far!
+Tomorrow we will play {sport} if the weather is ok.
+Today it has been raining cats and {animal} all day!
+If we cannot play {sport}, maybe we will just {verb}.
+I am sure it will be {adjective2} either way!
+See you soon! {name}
+""")
+
